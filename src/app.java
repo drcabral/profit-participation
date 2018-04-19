14c9d09970ee10d28ec9b548356778d43efb30c4
Diogo Cabral <drcabral@thoughtworks.com>
2018-04-19 13:57:57 -0300

    Added messages for invalid inputs.

--- src/app.java
+++ src/app.java
@@ -5,9 +5,24 @@ public class app {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of employees: ");
-        int totalEmployees = Integer.parseInt(scanner.nextLine());
+
+        int totalEmployees = 0;
+        try {
+            totalEmployees = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException ex) {
+            System.out.println("Enter a valid number of employees. " + ex.getLocalizedMessage());
+            return;
+        }
+
         System.out.print("Enter the company profit of this year: ");
-        double profit = Double.parseDouble(scanner.nextLine());
+
+        double profit = 0;
+        try {
+            profit = Double.parseDouble(scanner.nextLine());
+        } catch (NumberFormatException ex) {
+            System.out.println("Enter a valid profit. " + ex.getLocalizedMessage());
+            return;
+        }
 
         Company company = new Company(totalEmployees, profit);
 
@@ -15,7 +30,14 @@ public class app {
         System.out.print("Enter the employee occupation: ");
         String occupation = scanner.nextLine();
         System.out.print("Enter the employee performance: ");
-        int performance = Integer.parseInt(scanner.nextLine());
+
+        int performance = 0;
+        try {
+            performance = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException ex) {
+            System.out.println("Enter a valid profit. " + ex.getLocalizedMessage());
+            return;
+        }
 
         double totalProfitParticipation = company.getTotalProfitParticipation(occupation, performance);
 
